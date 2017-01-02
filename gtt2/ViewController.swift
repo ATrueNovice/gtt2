413059c2c3fdd8bc433f9e4b7d89286ad71bf548
ATrueNovice <sean.haughtonscott@gmail.com>
2017-01-02 14:15:25 -0500

    first

---
+++ gtt2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  gtt2
+//
+//  Created by New on 1/2/17.
+//  Copyright © 2017 HSI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("sean is dopeeee")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
