3f048a7a1feeaf756f724c0c170f7df9e3ef4a91
ATrueNovice <sean.haughtonscott@gmail.com>
2017-01-02 14:22:37 -0500

    second

--- gtt2/ViewController.swift
+++ gtt2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("sean is dopeeee")
+        print("hes extra dope yo")
     }
 
     override func didReceiveMemoryWarning() {
